625c262f6f73e39715f9d831fd9f1acad842e2cc
Andrii Sabinin <sabinin.andrii@gmail.com>
2025-08-25 20:57:12 +0200

    Project created

---
+++ InterfaceStore/InterfaceStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceStoreApp.swift
+//  InterfaceStore
+//
+//  Created by Andrii Sabinin on 25.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
