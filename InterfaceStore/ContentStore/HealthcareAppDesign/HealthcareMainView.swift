9741b5a397abacc997e9606df2e9550155c02a92
Andrii Sabinin <sabinin.andrii@gmail.com>
2025-09-05 00:06:15 +0200

    Add Appointment section with, refactoring
    - New Button Style for Appointment section
    - Stack effect for multiple appointments

--- InterfaceStore/ContentStore/HealthcareAppDesign/HealthcareMainView.swift
+++ InterfaceStore/ContentStore/HealthcareAppDesign/HealthcareMainView.swift
@@ -7,6 +7,35 @@
 
 import SwiftUI
 
+
+struct AppointmentButtonStyleFilled: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: 14))
+            .frame(maxWidth:.infinity)
+            .padding(16)
+            .foregroundStyle(.blue)
+            .background(Color.white)
+            .clipShape(Capsule())
+            .opacity(configuration.isPressed ? 0.7 : 1.0)
+            .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
+    }
+}
+
+struct AppointmentButtonStyleTransparent: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: 14))
+            .frame(maxWidth:.infinity)
+            .padding(16)
+            .foregroundStyle(.white)
+            .background(Color.white.opacity(0.2))
+            .clipShape(Capsule())
+            .opacity(configuration.isPressed ? 0.7 : 1.0)
+            .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
+    }
+}
+
 struct HealthcareMainView: View {
     
     @FocusState var isFocused
@@ -14,13 +43,19 @@ struct HealthcareMainView: View {
 
     var body: some View {
         ZStack {
-            Color("backgroundMain")
-                .ignoresSafeArea(edges: .all)
-            
             VStack {
                 headerView
-                Spacer()
+                ScrollView {
+                    LazyVStack {
+                        groupHeaderView
+                        appointmentCardView
+                    }
+                }
             }
+        }
+        .background {
+            Color("backgroundMain")
+                .ignoresSafeArea(edges: .all)
         }
     }
     
@@ -29,7 +64,6 @@ struct HealthcareMainView: View {
             profileView
                 .padding(.vertical, 10)
             searchView
-            Spacer()
         }
     }
     
@@ -121,6 +155,139 @@ struct HealthcareMainView: View {
             .frame(width: 64, height: 64)
             .padding(.trailing, 16)
         }
+    }
+    
+    var groupHeaderView: some View {
+        HStack {
+            Text("Recent Appointments")
+                .font(.system(size: 22))
+                .foregroundStyle(.black)
+            
+            Spacer()
+            Button {
+                // Action
+            } label: {
+                Text("See All")
+                    .foregroundStyle(.gray)
+            }
+        }
+        .frame(minHeight: 44)
+        .padding(.horizontal, 16)
+    }
+    
+    var appointmentCardView: some View {
+        ZStack {
+            
+            RoundedRectangle(cornerRadius: 24,
+                             style: .circular)
+            .fill(
+                Color("appointmentBackgound0")
+                    .mix(with: .white, by: 0.8)
+            )
+            .frame(maxWidth: .infinity)
+            .scaleEffect(0.90)
+            .offset(y: -30)
+            
+            RoundedRectangle(cornerRadius: 24,
+                             style: .circular)
+            .fill(
+                Color("appointmentBackgound0")
+                    .mix(with: .white, by: 0.6)
+            )
+            .frame(maxWidth: .infinity)
+            .scaleEffect(0.95)
+            .offset(y: -15)
+            
+            RoundedRectangle(cornerRadius: 24,
+                             style: .circular)
+            .fill(
+                LinearGradient(
+                    colors: [ Color("appointmentBackgound0"),
+                              Color("appointmentBackgound1") ],
+                    startPoint: .topLeading,
+                    endPoint: .bottom)
+            )
+            .frame(maxWidth: .infinity)
+            
+            VStack {
+                HStack {
+                    Image("female-doctor-avatar-white")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 70, height: 70)
+                        .background(Color.green)
+                        .clipShape(Circle())
+                        .padding(.leading, 16)
+                        .padding(.trailing, 14)
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Dr. Olivia Carter")
+                            .font(.system(size: 18))
+                            .foregroundStyle(.white)
+                        
+                        Text("Nephrology")
+                            .font(.system(size: 14))
+                            .foregroundStyle(.white)
+                    }
+                    Spacer()
+                    Button {
+                        // action
+                    } label: {
+                        Image(systemName: "envelope")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(14)
+                            .foregroundStyle(.white)
+                            .frame(width: 50, height: 50)
+                            .background(Color.white.opacity(0.2))
+                            .clipShape(Circle())
+                            .padding(14)
+                    }
+                    
+                }
+                
+                RoundedRectangle(cornerRadius: 1)
+                    .fill(Color.white.opacity(0.2))
+                    .frame(height: 1)
+                    .padding(.horizontal, 14)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Date")
+                            .font(.system(size: 16))
+                            .foregroundStyle(.white.opacity(0.5))
+
+                        Text("24 Apl. Monday")
+                            .font(.system(size: 14))
+                            .foregroundStyle(.white)
+                        
+                        Button("Rescedule") {
+                            // Action
+                        }
+                        .buttonStyle(AppointmentButtonStyleFilled())
+                    }
+                    Spacer()
+                    VStack(alignment: .leading) {
+                        Text("Time")
+                            .font(.system(size: 16))
+                            .foregroundStyle(.white.opacity(0.5))
+                        
+                        Text("10:00 AM - 11:00 AM")
+                            .font(.system(size: 14))
+                            .foregroundStyle(.white)
+                        
+                        Button("View profile") {
+                            // Action
+                        }
+                        .buttonStyle(AppointmentButtonStyleTransparent())
+
+                    }
+                }
+                .padding(.horizontal, 14)
+            }
+            .padding(.vertical, 16)
+        }
+        .padding(16)
     }
 }
 
